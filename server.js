275c0788d76f1fe7518abc33c4f3eb294c4e2795
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-06 13:26:15 +0000

    added middleware for body-parser

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
+var bodyParser = require('body-parser');
+
 
 var app = express();
 require('dotenv').load();
@@ -22,6 +24,11 @@ app.use(session({
 	resave: false,
 	saveUninitialized: true
 }));
+
+app.use(bodyParser.json());       
+app.use(bodyParser.urlencoded({    
+  extended: true
+})); 
 
 app.use(passport.initialize());
 app.use(passport.session());
