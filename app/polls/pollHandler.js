d46c31de1b554b6f231cebcded8a58b5b0641792
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-06 18:23:26 +0000

    updated pollHandler module to cater count array

--- app/polls/pollHandler.js
+++ app/polls/pollHandler.js
@@ -7,12 +7,26 @@ module.exports = {
   newPoll: function(data, user){
       var poll_data = {
           question: data.question,
-          options: {}
+          options: {},
+          count: [],
+          user: user
       }
       delete data['question'];
+      for(var key in data){
+        poll_data.count.push(0);
+      }
       poll_data.options = data;
-      poll_data.user = user.github;
       return new Poll(poll_data);
   },
+  
+  findMyPolls: function(user, res){
+    
+    Poll.find({
+      user: user
+    }, function (err, polls) {
+      if (err) return console.error(err);
+      res.send(polls);
+    })
+  }
     
 };
