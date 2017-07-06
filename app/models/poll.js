d7d6e5ee5e70f661ca32ccd636722252959dd82a
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-06 15:56:05 +0000

    added user field to poll model

--- app/models/poll.js
+++ app/models/poll.js
@@ -5,7 +5,15 @@ var Schema = mongoose.Schema;
 
 var Poll = new Schema({
     question: String,
-    options: { type: Schema.Types.Mixed, default: {} }
+    options: { type: Schema.Types.Mixed, default: {} },
+    user : {
+        github: {
+		    id: String,
+		    displayName: String,
+		    username: String,
+            publicRepos: Number
+	    }
+    }
  }, { minimize: false });
 
 module.exports = mongoose.model('Poll', Poll);
