8f848a963f34402d56e67af79c5e2b733f1ce15d
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-07 10:48:19 +0000

    added polls array to record all polls

--- app/controllers/pollController.my_polls.js
+++ app/controllers/pollController.my_polls.js
@@ -5,18 +5,16 @@
  var apiUrl = appUrl + '/my_polls/get';
  var poll_list = document.querySelector(".poll_list");
  var ctx = document.getElementById("myChart");
- var questions = [];
- var ids = [];
+ var  polls = [];
  
  ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, function(data){
      var id  = 0;
      JSON.parse(data).slice(0,-1).forEach(function(poll){
+         polls.push(poll);
          var btn = document.createElement("button");
          var delBtn = document.createElement("button");
          delBtn.innerHTML = "Delete Poll";
          btn.innerHTML = poll.question;
-         questions.push(poll.question);
-         ids.push(poll._id);
          btn.setAttribute("class", "btn btn-default btn-poll");
          btn.setAttribute("id", `${id}`);
          delBtn.setAttribute("class", "btn btn-danger btn-del");
@@ -31,12 +29,12 @@
      document.querySelectorAll(".btn-poll").forEach(btn => btn.addEventListener('click', function(e){
      var index = Number(e.target.getAttribute("id"));
      var myChart = new Chart(ctx, JSON.parse(data).slice(-1)[0][index]);
-     document.querySelector(".question").innerHTML = questions[index];
+     document.querySelector(".question").innerHTML = polls[index].question;
      }));
      
      document.querySelectorAll(".btn-del").forEach(btn => btn.addEventListener('click', function(e){
      var index = Number(e.target.getAttribute("id").slice(1));
-     ajaxFunctions.ajaxRequest('DELETE', apiUrl + `?id=${ids[index]}`, function (data) {
+     ajaxFunctions.ajaxRequest('DELETE', apiUrl + `?id=${polls[index]._id}`, function (data) {
         console.log(data);
         if(data === "success")
          window.location.reload(true);
