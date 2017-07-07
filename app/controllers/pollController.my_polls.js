81fcb0ef38b0797627f61066a800273ac4da3355
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-07 08:24:38 +0000

    chartHandler module that contains myChart function to create data for chart.js

--- app/controllers/pollController.my_polls.js
+++ app/controllers/pollController.my_polls.js
@@ -6,16 +6,23 @@
  var poll_list = document.querySelector(".poll_list");
  
  ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, function(data){
-     console.log(JSON.parse(data));
-     JSON.parse(data).forEach(function(poll){
+     var id  = 0;
+     JSON.parse(data).slice(0,-1).forEach(function(poll){
          var btn = document.createElement("button");
          btn.innerHTML = poll.question;
-         btn.setAttribute("class", "btn btn-default");
+         btn.setAttribute("class", "btn btn-default btn-poll");
+         btn.setAttribute("id", `${id}`);
+         id++;
          poll_list.append(btn);
          poll_list.append(document.createElement("br"));
          poll_list.append(document.createElement("br"));
      });
      
+    var ctx = document.getElementById("myChart");
+    document.querySelectorAll(".btn-poll").forEach(btn => btn.addEventListener('click', function(e){
+     console.log(document.querySelector(".btn-poll"));
+    var myChart = new Chart(ctx, JSON.parse(data).slice(-1)[0][Number(e.target.getAttribute("id"))]);
+}));
  }));
  
 })();
