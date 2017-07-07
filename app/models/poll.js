053b45fe7b4998e364c28c2415ef8677a97213bf
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-07 14:09:41 +0000

    added voters array to recored voter details

--- app/models/poll.js
+++ app/models/poll.js
@@ -7,7 +7,8 @@ var Poll = new Schema({
     question: String,
     options: { type: Schema.Types.Mixed, default: {} },
     count: [],
-    user : {type: Schema.Types.Mixed, default: {} }
+    user : {type: Schema.Types.Mixed, default: {} },
+    voters: []
  }, { minimize: false });
 
 module.exports = mongoose.model('Poll', Poll);
