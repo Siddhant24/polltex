90e78764f0986a39b4eb67285405ae7c44b30514
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-08 09:53:42 +0000

    added Add new option button and input field

--- app/controllers/pollController.all_polls.js
+++ app/controllers/pollController.all_polls.js
@@ -11,15 +11,29 @@
  var prevChart = null;
  
  ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, function(data){
+     console.log(data);
       var id  = 0;
-      JSON.parse(data).slice(0,-1).forEach(function(poll){
+      JSON.parse(data).poll_data.slice(0,-1).forEach(function(poll){
+          console.log(poll);
          polls.push(poll);
          var btn = document.createElement("button");
+         var option_btn = document.createElement("button");
+         var input = document.createElement('input');
+         input.setAttribute("type", "text");
+         input.setAttribute("id", `^${id}`);
+         input.setAttribute("style", "display:none");
+         option_btn.innerHTML = "Add a new option";
          btn.innerHTML = poll.question;
          btn.setAttribute("class", "btn btn-default btn-poll");
          btn.setAttribute("id", `${id}`);
+         option_btn.setAttribute("class", "btn btn-info btn-option");
+         option_btn.setAttribute("id", `-${id}`);
          id++;
          poll_list.append(btn);
+         if(JSON.parse(data).isAuthenticated){
+             poll_list.append(input);
+             poll_list.append(option_btn);
+         }
          poll_list.append(document.createElement("br"));
          poll_list.append(document.createElement("br"));
      });
@@ -27,7 +41,7 @@
      document.querySelectorAll(".btn-poll").forEach(btn => btn.addEventListener('click', function(e){
      if(prevChart) prevChart.destroy();
      var index = Number(e.target.getAttribute("id"));
-     var myChart = new Chart(ctx, JSON.parse(data).slice(-1)[0][index]);
+     var myChart = new Chart(ctx, JSON.parse(data).poll_data.slice(-1)[0][index]);
      prevChart = myChart;
      document.querySelector(".question").innerHTML = polls[index].question;
      Object.keys(polls[index].options).forEach(function(option){
@@ -51,6 +65,25 @@
        window.location.reload();
       });
      });
+     
+     if(JSON.parse(data).isAuthenticated){
+         document.querySelectorAll(".btn-option").forEach(option => option.addEventListener("click", function(e){
+             var id_num= Number(e.target.getAttribute("id").slice(1));
+             var input = document.getElementById(`^${id_num}`);
+             input.removeAttribute("style");
+             e.target.innerHTML = "Submit new option";
+             if(input.value != ""){
+                var body = {};
+                body.poll_id = polls[id_num]._id;
+                body.options = polls[id_num].options;
+                body.options[`option${Object.keys(polls[id_num].options).length+1}`] = input.value;
+                console.log(body);
+                ajaxFunctions.ajaxPostRequest(body,appUrl + '/all_polls/get', function(data){
+                    window.location.reload();
+                });
+             }
+         }));
+     }
      
  }));
  
